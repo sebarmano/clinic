99a29cd60becc56a98d9daa317586403ca4ea01b
Sebastian Armano <sebarmano@gmail.com>
2021-02-24 14:47:49 +1100

    Add tailwind 2.0

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [
+    './app/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
