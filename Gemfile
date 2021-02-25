4b1b5c72a60288b79edb2e4ee176eec44ccc598c
Sebastian Armano <sebarmano@gmail.com>
2021-02-25 22:13:32 +1100

    Add seeds

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ group :development, :test do
 end
 
 group :development do
+  gem "faker", "~> 2.16.0"
   gem "listen", "~> 3.3"
   gem "rack-mini-profiler", "~> 2.0"
   gem "web-console", ">= 4.1.0"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Adding customers..."
+10.times do
+  Customer.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    phone: Faker::PhoneNumber.phone_number,
+    email: Faker::Internet.email,
+    address: Faker::Address.full_address,
+  )
+end
+
+puts "Adding vehicles..."
+20.times do
+  Vehicle.create(
+    customer: Customer.all.sample,
+    brand: Faker::Vehicle.make,
+    model: Faker::Vehicle.model,
+    year: Faker::Vehicle.year,
+    plate: Faker::Vehicle.license_plate,
+  )
+end
+puts "Done."
